d66b3f515efe55c4229e2e9d5e98fed0c0613613
MantorXI <140944564+MantorXI@users.noreply.github.com>
2025-11-14 20:10:49 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,3 @@
-// =======================================================
-//  您的个人网站数据库
-//  从今以后，您只需要在这里修改和添加网站信息即可
-// =======================================================
-
 const webstackData = [
     {
         "category": "常用推荐",
@@ -15,15 +10,26 @@ const webstackData = [
             },
             {
                 "name": "Z-Library",
-                "url": "https://singlelogin.re/",
+                "url": "https://zh.z-library.sk/",
                 "desc": "全球最大的数字图书馆之一。",
                 "icon": "../assets/images/logos/zlibrary.png"
             },
             {
                 "name": "88看球",
-                "url": "https://www.88kanqiu.net/",
+                "url": "http://www.88kq.net/",
                 "desc": "88看球地址发布页",
-                "icon": "../assets/images/logos/88kanqiu.png" // 假设图标路径
+                "icon": "../assets/images/logos/bilibili.png"
+            }
+        ]
+    },
+    {
+        "category": "趣味网站",
+        "sites": [
+            {
+                "name": "Keybr",
+                "url": "https://www.keybr.com/",
+                "desc": "在线打字练习和竞赛网站。",
+                "icon": "../assets/images/logos/keybr.png"
             }
         ]
     },
@@ -35,23 +41,6 @@ const webstackData = [
                 "url": "https://www.bilibili.com/",
                 "desc": "国内知名的视频弹幕网站。",
                 "icon": "../assets/images/logos/bilibili.png"
-            }
-        ]
-    },
-    {
-        "category": "资源网站",
-        "sites": [
-            // 把“资源网站”分类下的网站放在这里
-        ]
-    },
-    {
-        "category": "趣味网站",
-        "sites": [
-            {
-                "name": "Keybr",
-                "url": "https://www.keybr.com/zh-hans/index",
-                "desc": "在线打字练习和竞赛网站。",
-                "icon": "../assets/images/logos/keybr.png"
             }
         ]
     }
