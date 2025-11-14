3613c58b2ee3369089a1e3483d3e3fa67aca5def
MantorXI <140944564+MantorXI@users.noreply.github.com>
2025-11-14 18:16:01 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,58 @@
+// =======================================================
+//  您的个人网站数据库
+//  从今以后，您只需要在这里修改和添加网站信息即可
+// =======================================================
+
+const webstackData = [
+    {
+        "category": "常用推荐",
+        "sites": [
+            {
+                "name": "YouTube",
+                "url": "https://www.youtube.com/",
+                "desc": "全球最大的视频分享平台。",
+                "icon": "../assets/images/logos/youtube.png"
+            },
+            {
+                "name": "Z-Library",
+                "url": "https://singlelogin.re/",
+                "desc": "全球最大的数字图书馆之一。",
+                "icon": "../assets/images/logos/zlibrary.png"
+            },
+            {
+                "name": "88看球",
+                "url": "https://www.88kanqiu.net/",
+                "desc": "88看球地址发布页",
+                "icon": "../assets/images/logos/88kanqiu.png" // 假设图标路径
+            }
+        ]
+    },
+    {
+        "category": "视频网站",
+        "sites": [
+            {
+                "name": "Bilibili",
+                "url": "https://www.bilibili.com/",
+                "desc": "国内知名的视频弹幕网站。",
+                "icon": "../assets/images/logos/bilibili.png"
+            }
+        ]
+    },
+    {
+        "category": "资源网站",
+        "sites": [
+            // 把“资源网站”分类下的网站放在这里
+        ]
+    },
+    {
+        "category": "趣味网站",
+        "sites": [
+            {
+                "name": "Keybr",
+                "url": "https://www.keybr.com/zh-hans/index",
+                "desc": "在线打字练习和竞赛网站。",
+                "icon": "../assets/images/logos/keybr.png"
+            }
+        ]
+    }
+];
