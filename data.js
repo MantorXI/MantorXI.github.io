8576bca32f120732ba77979ae12ffcb186859ddb
MantorXI <140944564+MantorXI@users.noreply.github.com>
2025-11-15 16:54:38 +0800

    Update data.js

--- data.js
+++ data.js
@@ -18,7 +18,7 @@ const webstackData = [
                 "name": "88看球",
                 "url": "http://www.88kq.net/",
                 "desc": "88看球地址发布页",
-                "icon": "../assets/images/logos/bilibili.png"
+                "icon": "../assets/images/logos/88kanqiu.png"
             }
         ]
     },
@@ -41,17 +41,114 @@ const webstackData = [
                 "url": "https://www.bilibili.com/",
                 "desc": "国内知名的视频弹幕网站。",
                 "icon": "../assets/images/logos/bilibili.png"
+            },
+            {
+                "name": "IYF.TV",
+                "url": "https://www.iyf.tv/",
+                "desc": "视频类网站。",
+                "icon": "../assets/images/logos/iyftv.png"
+            },
+            {
+                "name": "Libvio",
+                "url": "https://www.libvio.link",
+                "desc": "提供剧集、动漫、综艺等影视内容。",
+                "icon": "../assets/images/logos/libvio.png"
+            },
+            {
+                "name": "七味网",
+                "url": "https://www.qn63.com/",
+                "desc": "一站式高清影视资源平台，支持免费在线观看电影、电视剧和动漫。",
+                "icon": "../assets/images/logos/qwei.png"
             }
         ]
     },
     {
-        "category": "资源网站",
+        "category": "软件下载",
         "sites": [
             {
-                "name": "Z-Library",
-                "url": "https://zh.z-library.sk/",
-                "desc": "全球最大的数字图书馆之一。",
-                "icon": "../assets/images/logos/zlibrary.png"
+                "name": "大眼仔旭",
+                "url": "https://www.dayanzai.me/",
+                "desc": "提供各类PC软件、绿色软件、手机游戏、安卓APP、汉化软件及软件教程的下载站点。",
+                "icon": "../assets/images/logos/dayanzai.png"
+            },
+            {
+                "name": "异星软件空间",
+                "url": "https://www.yxssp.com/",
+                "desc": "非营利软件应用库，提供学习用途的软件分享和资源下载。",
+                "icon": "../assets/images/logos/yxssp.png"
+            }
+        ]
+    },
+    {
+        "category": "游戏下载",
+        "sites": [
+            {
+                "name": "Switch520",
+                "url": "https://www.520switch.com/",
+                "desc": "专业的Switch游戏下载平台，提供NS免费游戏资源及破解游戏。",
+                "icon": "../assets/images/logos/switch520.png"
+            }
+        ]
+    },
+    {
+        "category": "代理工具",
+        "sites": [
+            {
+                "name": "一元机场",
+                "url": "https://一元机场.asia",
+                "desc": "廉价VPN机场服务，支持Trojan协议，直连节点，低成本上网加速。",
+                "icon": "../assets/images/logos/1yuanjichang.png"
+            },
+            {
+                "name": "流量光机场",
+                "url": "https://llg01.com/",
+                "desc": "性价比高的VPN机场，支持Clash、V2ray，高速稳定节点覆盖热门国家。",
+                "icon": "../assets/images/logos/llgjichang.png"
+            },
+            {
+                "name": "一元机场",
+                "url": "https://103.212.99.243:51122/",
+                "desc": "疑似VPN机场登录仪表盘，提供上网加速服务。",
+                "icon": "../assets/images/logos/yiyuanjichang.png"
+            }
+        ]
+    },
+    {
+        "category": "工具",
+        "sites": [
+            {
+                "name": "GitHub 文件加速代理",
+                "url": "https://gh-proxy.com/",
+                "desc": "快速访问GitHub Releases、Raw等资源的代理工具，支持大文件下载和自动缓存。",
+                "icon": "../assets/images/logos/ghproxy.png"
+            }
+        ]
+    },
+    {
+        "category": "论坛",
+        "sites": [
+            {
+                "name": "NodeSeek",
+                "url": "https://www.nodeseek.com/",
+                "desc": "主机爱好者社区论坛，讨论服务器新闻、优惠、开发经验及开源工具。",
+                "icon": "../assets/images/logos/nodeseek.png"
+            }
+        ]
+    },
+    {
+        "category": "导航",
+        "sites": [
+            {
+                "name": "摸鱼导航",
+                "url": "https://n.mumingfang.com/my",
+                "desc": "摸鱼爱好者导航，提供各种闲逛网站、工具和小游戏。",
+                "icon": "../assets/images/logos/moyuhu.png"
+            },
+            {
+                "name": "霂明导航",
+                "url": "https://n.mumingfang.com/",
+                "desc": "创意工作者的优质网址导航，涵盖AI、设计、前端、站长等分类。",
+                "icon": "../assets/images/logos/muming.png"
             }
         ]
     }
